e700ec7b2e54144726be528fcf809fef024f3bbc
JoseVRueda <94568204+JoseVRueda@users.noreply.github.com>
2023-05-02 22:48:24 +0000

    modified:   Compresor.py modified:   Descompresor.py modified:   Verificador.py

--- Compresor.py
+++ Compresor.py
@@ -7,7 +7,7 @@ start_time = time.time()
 # Se crea una lista de caracteres que va a tener cada linea del archivo a comprimir (Lines),
 # luego usamos otra lista para guardar cada letra (my_string)
 Lines = []
-for line in open(r"/workspaces/Compresor_Descompresor/Archivos/LaBiblia.txt", 'r', encoding="latin-1").readlines():
+for line in open("./Archivos/LaBiblia.txt", 'r', encoding="latin-1").readlines():
   Lines.append(line)
 my_string=[]
 for h in Lines:
@@ -146,7 +146,7 @@ for i in range(8):
 pint = bin_to_base64(bitstring)
 
 # Generar archivo comprimido
-fic = open(r"/workspaces/Compresor_Descompresor/Archivos/comprimido.elmejorprofesor", "w", encoding="latin-1")
+fic = open("./Archivos/comprimido.elmejorprofesor", "w", encoding="latin-1")
 fic.write(arbol+id+"@"+pint)
 fic.close()
 
--- Verificador.py
+++ Verificador.py
@@ -4,10 +4,10 @@
 ###############################################################################################
 
 Lines = []
-for line in open(r"/workspaces/Compresor_Descompresor/Archivos/LaBiblia.txt", 'r', encoding="latin-1").readlines():
+for line in open("./Archivos/LaBiblia.txt", 'r', encoding="latin-1").readlines():
   Lines.append(line)
 Lines2 = []
-for line in open(r"/workspaces/Compresor_Descompresor/Archivos/descomprimido-elmejorprofesor.txt", 'r', encoding="latin-1").readlines():
+for line in open("./Archivos/descomprimido-elmejorprofesor.txt", 'r', encoding="latin-1").readlines():
   Lines2.append(line)
 if(Lines == Lines2):
   print("Ok")
